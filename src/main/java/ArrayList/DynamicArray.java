d37dc4b7d9eeeabfce857e5d2028e535c3093b7c
Sirazad <lajosimola2@gmail.com>
2021-03-15 22:38:31 +0100

    set up project tasks
    added the tests and the base method structure of the project.
    
    The task is to write a code that makes it function and pass all tests.

---
+++ src/main/java/ArrayList/DynamicArray.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+
+public class DynamicArray {
+
+    private int[] array;
+    private int size;
+
+    public DynamicArray(int capacity) {
+    }
+
+    public DynamicArray() {
+    }
+
+    public int getCapacity() {
+            return array.length;
+        }
+    }
+
+    public void add(int value) {
+    }
+
+    public int get(int index) {
+    }
+
+    public void remove(int indexToBeRemoved) {
+    }
+
+    public void insert(int index, int newValue) {
+    }
+
+    @Override
+    public String toString() {
+    }
+
+}
+
+
