fe06d86f2326a46d421f5982127c5917b1e83430
Sirazad <lajosimola2@gmail.com>
2021-03-16 23:54:59 +0100

    turn elements into String by toString
    in order to avoid printing reference instead of content

--- src/main/java/LinkedList/SinglyLinkedList.java
+++ src/main/java/LinkedList/SinglyLinkedList.java
@@ -72,7 +72,20 @@ public class SinglyLinkedList<T> {
      */
 
     public String toString() {
-        return null;
+        int size = size();
+        tail = head;
+        String list = "[";
+        if (size == 0) {
+            return "[]";
+        } else {
+            for (int i = 0; i < size; i++) {
+                list += tail.getValue();
+                if (tail.getNext()!= null) list += ", ";
+                tail = tail.getNext();
+            }
+            list += "]";
+        }
+        return list;
     }
 
 
